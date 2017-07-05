7000f3c8b8a82f0e12beb4b183b950fb94644198
aurelianeLucas <aureliane.lucas@sogeti.com>
2017-07-05 10:57:22 +0200

    Demo api rest sqlalchemy

--- rest/car_ressource.py
+++ rest/car_ressource.py
@@ -1,38 +1,52 @@
-import persistence.car_persistence as commons_car_service
 import commons.commons_utilitaire as commons_utilitaire
-from bottle import get, post, put, delete, request, response, hook, run
-from json import dumps
+from bottle import get, post, put, delete, request, response, hook, view, template
+from services import car_service as commons_car_service
 from entities.car import Car
+from json import dumps
+import bottle
 
-car_service = commons_car_service.CarPersistence(Car)
-invalid_parameters = "Invalid parameters"
+car_service = commons_car_service.CarService("car")
+invalid_parameters = "Invalid arguments"
+entity_not_found = "Entity not found"
+
+
+@hook('after_request')
+def init_response():
+    response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
 
 @get('/api/v1/cars')
 def get_all():
-    response.status = 200
-    cars = car_service.find_all()
-    if cars is None:
-        response.status = 404
-    return dumps(cars, default=commons_utilitaire.jdefault)
+    try:
+        response.status = 200
+        cars = car_service.find_all()
+        list = [car.to_dict() for car in cars]
+        return dumps(list)
+    except TypeError:
+        return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
 @get('/api/v1/cars/<car_id>')
 def get_by_id(car_id):
-    response.status = 200
-    car = car_service.find_by_id(car_id)
-    if car is None:
-        response.status = 404
-    return dumps(car, default=commons_utilitaire.jdefault)
+    try:
+        response.status = 200
+        car = car_service.find_by_id(car_id)
+        return commons_utilitaire.json_response(car, entity_not_found)
+    except TypeError as e:
+        print(e)
+        return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
 @post('/api/v1/cars')
 def create_car():
     try:
+        response.status = 200
         car = commons_utilitaire.get_record_from_body(request, Car)
-        response.status = 201
-        return dumps(commons_car_service.create(car), default=commons_utilitaire.jdefault)
-    except TypeError:
+        result = car_service.create(car)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
@@ -41,19 +55,19 @@ def update_car():
     try:
         response.status = 200
         car = commons_utilitaire.get_record_from_body(request, Car)
-        result = commons_car_service.update(car)
-    except TypeError:
+        result = car_service.update(car)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
-    if not result:
-        return commons_utilitaire.error_handler(404, "identifiant not find", response)
 
 
 @delete('/api/v1/cars/<car_id>')
 def delete_car(car_id):
     try:
         response.status = 200
-        result = commons_car_service.delete_by_id(car_id)
-    except TypeError:
+        result = car_service.delete_by_id(car_id)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
-    if not result:
-        return commons_utilitaire.error_handler(404, "identifiant not find", response)
--- rest/driver_ressource.py
+++ rest/driver_ressource.py
@@ -1,11 +1,12 @@
-import persistence.driver_persistence as commons_driver_service
+from json import dumps
 import commons.commons_utilitaire as commons_utilitaire
 from bottle import get, post, put, delete, request, response, hook
-from json import dumps
+from services import driver_service as commons_driver_service
 from entities.driver import Driver
 
-driver_service = commons_driver_service.DriverPersistence(Driver)
-invalid_parameters = "Invalid parameters"
+driver_service = commons_driver_service.DriverService("driver")
+invalid_parameters = "Invalid arguments"
+entity_not_found = "Entity not found"
 
 
 @hook('after_request')
@@ -16,29 +17,35 @@ def init_response():
 
 @get('/api/v1/drivers')
 def get_all():
-    response.status = 200
-    drivers = driver_service.find_all()
-    if drivers is None:
-        response.status = 404
-    return dumps(drivers, default=commons_utilitaire.jdefault)
+    try:
+        response.status = 200
+        drivers = driver_service.find_all()
+        list = [driver.to_dict() for driver in drivers]
+        return dumps(list)
+    except TypeError:
+        return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
 @get('/api/v1/drivers/<driver_id>')
 def get_by_id(driver_id):
-    response.status = 200
-    driver = driver_service.find_by_id(driver_id)
-    if driver is None:
-        response.status = 404
-    return dumps(driver, default=commons_utilitaire.jdefault)
+    try:
+        response.status = 200
+        driver = driver_service.find_by_id(driver_id)
+        return commons_utilitaire.json_response(driver, entity_not_found)
+    except TypeError as e:
+        print(e)
+        return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
 @post('/api/v1/drivers')
 def create_driver():
     try:
+        response.status = 200
         driver = commons_utilitaire.get_record_from_body(request, Driver)
-        response.status = 201
-        return dumps(commons_driver_service.create(driver), default=commons_utilitaire.jdefault)
-    except TypeError:
+        result = driver_service.create(driver)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
 
 
@@ -47,20 +54,19 @@ def update_driver():
     try:
         response.status = 200
         driver = commons_utilitaire.get_record_from_body(request, Driver)
-        result = commons_driver_service.update(driver)
-    except TypeError:
+        result = driver_service.update(driver)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
-    if not result:
-        return commons_utilitaire.error_handler(404, "identifiant not find", response)
 
 
 @delete('/api/v1/drivers/<driver_id>')
 def delete_driver(driver_id):
     try:
         response.status = 200
-        result = commons_driver_service.delete_by_id(driver_id)
-    except TypeError:
+        result = driver_service.delete_by_id(driver_id)
+        return commons_utilitaire.json_bool_response(result)
+    except TypeError as e:
+        print(e)
         return commons_utilitaire.error_handler(400, invalid_parameters, response)
-    if not result:
-        return commons_utilitaire.error_handler(404, "identifiant not find", response)
-
