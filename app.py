09d454cff7e77ee2964b3a65acb2c98ae4f25a91
aurelianeLucas <aureliane.lucas@sogeti.com>
2017-07-11 09:41:10 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import bottle
 from bottle import run
+import bottle
 import rest.car_ressource
 import rest.driver_ressource
 
 if __name__ == "__main__":
     run(host='localhost', port=3000)
-    app = bottle.default_app()
+
+app = bottle.default_app()
